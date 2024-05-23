3aeadcdc46e78ed660c07f58fb3e659edb0cb921
Frothy <76622149+FrothyMarrow@users.noreply.github.com>
2024-05-23 17:21:37 -0400

    Change starship prompt

--- home/starship.nix
+++ home/starship.nix
@@ -2,4 +2,11 @@
   programs.starship.enable = true;
 
   programs.starship.enableZshIntegration = true;
+
+  programs.starship.settings = {
+    character = {
+      success_symbol = "[➜](green) ";
+      error_symbol = "[➜](red) ";
+    };
+  };
 }
