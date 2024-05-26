558b3243e3f8e1e8da4ad4863bda84ef4208f01b
Frothy <76622149+FrothyMarrow@users.noreply.github.com>
2024-05-26 18:06:46 -0400

    Add custom alice carbon extension to nixpkgs

--- darwin/overlays.nix
+++ darwin/overlays.nix
@@ -1,10 +1,18 @@
 {
+  pkgs,
+  lib,
+  ...
+}: {
   nixpkgs.overlays = [
     (final: prev: {
       vencord = prev.vencord.overrideAttrs {
         patches = [
           ../patches/VencordConfigDir.patch
         ];
+      };
+
+      vscode-extensions = lib.recursiveUpdate prev.vscode-extensions {
+        frothymarrow.alice-carbon = pkgs.callPackage ../pkgs/alice-carbon.nix {};
       };
     })
   ];
