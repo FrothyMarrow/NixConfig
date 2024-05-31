570c869f7096fbd0f4b99d084f85999070eb7dc3
Frothy <76622149+FrothyMarrow@users.noreply.github.com>
2024-05-31 19:32:17 -0400

    Remove unused packages from home manager

--- home/packages.nix
+++ home/packages.nix
@@ -1,22 +1,14 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    obsidian
     element-desktop
     qbittorrent
     zathura
     mpv
-    bitwarden-cli
-    linode-cli
-    onefetch
-    lazygit
     ripgrep
     fd
-    comma
     lsd
-    tree
     (nerdfonts.override {
       fonts = [
-        "Meslo"
         "Iosevka"
       ];
     })
