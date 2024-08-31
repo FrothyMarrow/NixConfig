bdc7e67a4171d4cf0c3243d7b0cd4bd0b89c4ef0
Frothy <76622149+FrothyMarrow@users.noreply.github.com>
2024-08-30 23:46:14 -0400

    Use WebGPU renderer in Wezterm to workaround borken rendering with OpenGL

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -8,6 +8,7 @@
     config.font_size = 17
     config.color_scheme_dirs = { '${inputs.wezterm-oxocarbon}' }
     config.color_scheme = 'Oxocarbon Dark'
+    config.front_end = "WebGpu"
     config.window_decorations = 'RESIZE'
     config.window_background_opacity = 0.96
     config.macos_window_background_blur = 50
